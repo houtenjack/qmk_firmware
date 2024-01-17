155ef2b05c63391fa48813147d4a74ce17f249a3
Jack <htnjack@proton.me>
2024-01-17 20:25:23 +0100

    dvorak with gui helpers

--- keyboards/splitkb/aurora/corne/keymaps/htndvorak/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/htndvorak/keymap.c
@@ -11,12 +11,14 @@ enum layers {
     _NUM,
     _SYM,
     _FN,
+    _GUI,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_DEFAULT] = LAYOUT_split_3x6_3(KC_TAB, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSPC, KC_ESC, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH, KC_LSFT, KC_QUOT, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RALT, KC_LGUI, MO(2), KC_SPC, KC_ENT, MO(1), KC_RCTL),
-                                                              [_NUM]     = LAYOUT_split_3x6_3(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RALT, KC_LGUI, MO(3), KC_SPC, KC_ENT, KC_TRNS, KC_RCTL),
-                                                              [_SYM]     = LAYOUT_split_3x6_3(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_PIPE, KC_BSPC, KC_ESC, KC_PLUS, KC_LBRC, KC_LCBR, KC_LPRN, KC_BSLS, KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC, KC_MINS, KC_GRV, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RALT, KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, MO(3), KC_RCTL),
-                                                              [_FN]      = LAYOUT_split_3x6_3(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_MPLY, KC_MSTP, KC_NO, KC_NO, KC_NO, KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, KC_TRNS, KC_RCTL)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_DEFAULT] = LAYOUT_split_3x6_3(KC_TAB, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSPC, KC_ESC, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH, KC_LSFT, KC_QUOT, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RALT, MO(4), MO(2), KC_SPC, KC_ENT, MO(1), KC_RCTL),
+                                                              [_NUM]     = LAYOUT_split_3x6_3(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_DEL, KC_LSFT, KC_NO, KC_NO, KC_PGDN, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_LGUI, MO(3), KC_SPC, KC_ENT, KC_TRNS, KC_RCTL),
+                                                              [_SYM]     = LAYOUT_split_3x6_3(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_ESC, KC_GRV, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS, KC_GRV, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RCTL, KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, MO(3), KC_RALT),
+                                                              [_FN]      = LAYOUT_split_3x6_3(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_PGDN, KC_PGUP, KC_NO, KC_MUTE, KC_MPLY, KC_MSTP, KC_NO, KC_NO, KC_RCTL, KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, KC_TRNS, KC_RALT),
+                                                              [_GUI]     = LAYOUT_split_3x6_3(LGUI(KC_TAB), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_F), KC_NO, LGUI(KC_C), LGUI(KC_R), KC_NO, KC_NO, LGUI(KC_ESC), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_LSFT), KC_NO, LGUI(KC_Q), LGUI(KC_O), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, LGUI(KC_SPC), LGUI(KC_ENT), KC_NO, LGUI(KC_RCTL))};
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -53,32 +55,32 @@ static void render_layer_block(void) {
         default:
             oled_write_ln_P(PSTR("Undefined"), false);
     }
-    oled_write_ln_P(PSTR(""), false);
+    oled_write_ln_P(PSTR(" "), false);
 }
 
 static void render_mod(uint8_t mod, uint8_t mask, char *name) {
     if (mod & mask) {
         oled_write_ln_P(PSTR(name), false);
     } else {
-        oled_write_ln_P(PSTR(""), false);
+        oled_write_ln_P(PSTR(" "), false);
     }
 }
 
 static void render_mod_block(void) {
     uint8_t mod = get_mods() | get_oneshot_mods();
     render_mod(mod, MOD_MASK_CTRL, "CTRL");
-    render_mod(mod, MOD_MASK_SHIFT, "SHIFT");
+    render_mod(mod, MOD_MASK_SHIFT, "SHFT");
     render_mod(mod, MOD_MASK_ALT, "ALT");
     render_mod(mod, MOD_MASK_GUI, "GUI");
-    oled_write_ln_P(PSTR(""), false);
+    oled_write_ln_P(PSTR(" "), false);
 }
 
 static void render_master(void) {
     render_layer_block();
     render_mod_block();
     oled_write_ln(read_keylogs(), false);
-    oled_write_ln_P(PSTR(""), false);
-    oled_write_ln_P(PSTR(""), false);
+    oled_write_ln_P(PSTR(" "), false);
+    oled_write_ln_P(PSTR(" "), false);
 }
 
 static void render_logo(void) {
